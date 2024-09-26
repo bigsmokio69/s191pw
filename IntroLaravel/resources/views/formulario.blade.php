e4733b269d59a5e41ab768451937b33bdeb1b365
bigsmokio69 <jecs1704cs@gmail.com>
2024-09-26 14:38:28 -0600

    Avance de la práctica 3

---
+++ IntroLaravel/resources/views/formulario.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite(['resources/js/app.js'])
+    <title>Registro clientes</title>
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg-dark bg-body-tertiary ">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Turista sin maps</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Formulario de registro</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Consultar clientes</a>
+                    </li>
+                </li>
+                </ul>
+                <form class="d-flex" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
+            </div>
+        </div>
+    </nav>
+    <h1>Formulario</h1>
+</body>
+
+</html>
--- IntroLaravel/routes/web.php
+++ IntroLaravel/routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+/* Route::get('/', function () {
     return view('welcome');
-});
+}); */
+
+Route::view('/', 'welcome'); //En este tipo de ruta view lo primero es la ruta y el segundo parametro es el nombre del archivo
+
+Route::view('/formulario', 'formulario');
