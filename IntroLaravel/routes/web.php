42875d90b5e6b42aa4b45a599219a7ec0a0f45db
bigsmokio69 <jecs1704cs@gmail.com>
2024-10-11 20:25:22 -0600

    Practica 4 controladores

--- IntroLaravel/routes/web.php
+++ IntroLaravel/routes/web.php
@@ -1,13 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\controladorVistas;
 
 /* Route::get('/', function () {
     return view('welcome');
 }); */
 
-Route::view('/', 'inicio')->name('raiz'); //En este tipo de ruta view lo primero es la ruta y el segundo parametro es el nombre del archivo
+/* Route::view('/', 'inicio')->name('raiz'); //En este tipo de ruta view lo primero es la ruta y el segundo parametro es el nombre del archivo
 
 Route::view('/formulario', 'formulario')->name('rutacacas');
-Route::view('/consultar', 'clientes')->name('rutaclientes');
+Route::view('/consultar', 'clientes')->name('rutaclientes'); */
+
+Route::get('/', [controladorVistas::class,'home'])->name('raiz');
+Route::get('/formulario', [controladorVistas::class, 'insert'])->name('rutacacas');
+Route::get('/consultar', [controladorVistas::class, 'select'])->name('rutaclientes');
+
 Route::view('/component', 'componentes')->name('rutacomps');
