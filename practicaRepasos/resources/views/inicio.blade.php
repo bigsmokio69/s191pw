231dc91701fbc0c4fa2e13aec47c58190ea78c81
bigsmokio69 <jecs1704cs@gmail.com>
2024-10-17 20:21:32 -0600

    Repaso 1 Paso 3 creación de la portada
    PD: En el commit anterior se me fué el avión y puse práctica 5

---
+++ practicaRepasos/resources/views/inicio.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite(['resources/js/app.js'])
+    <title>Document</title>
+</head>
+
+<body>
+
+
+    <div class="d-flex flex-column justify-content-center align-items-center full-height mt-5">
+        <img src="{{ asset('images/lainpfp.jpg') }}" alt="">
+        
+        <div class="mt-5 d-flex flex-column justify-content-center align-items-center full-height" >
+        <h1>HOLA MUNDO REPASO 1</h1>
+        <h2>Práctica 1 de Repaso</h2>
+        <h2>Programación Web</h2>
+        <h2>Jorge Emiliano Cortes Sánchez</h2>
+        <h2>17 de Octubre de 2024</h2>
+        </div>
+    </div>
+
+</body>
+
+</html>
