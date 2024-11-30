3bc106bf4d7792953f03a857d450e55b09bc31f3
bigsmokio69 <jecs1704cs@gmail.com>
2024-11-29 23:01:07 -0600

    Fin de la practica 10

--- IntroLaravelE/app/Http/Controllers/ClienteController.php
+++ IntroLaravelE/app/Http/Controllers/ClienteController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\cliente;
 use Illuminate\Http\Request;
 
-class ClienteController extends Controller
+class clienteController extends Controller
 {
     public function home(){
         return view('inicio');
@@ -13,7 +13,8 @@ class ClienteController extends Controller
     
     public function index()
     {
-        return view('clientes');
+        $consulta=cliente::all();
+        return view('clientes', compact('consulta'));
     }
 
     /**
@@ -29,7 +30,18 @@ class ClienteController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $addCliente= new cliente();
+
+        $addCliente->nombre=$request->input('txtnombre');
+        $addCliente->apellido=$request->input('txtapellido');
+        $addCliente->correo=$request->input('txtcorreo');
+        $addCliente->telefono=$request->input('txttelefono');
+
+        $addCliente->save();
+
+        $usuario=$request->input('txtnombre');
+        session()->flash('exito', 'Se guardo correctamente al usuario '.$usuario);
+        return redirect()->back();
     }
 
     /**
@@ -51,16 +63,35 @@ class ClienteController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, cliente $cliente)
+    public function update(Request $request, $id)
     {
-        //
+        $upCliente= cliente::find($id);
+
+        $upCliente->nombre=$request->input('nombre');
+        $upCliente->apellido=$request->input('apellido');
+        $upCliente->correo=$request->input('correo');
+        $upCliente->telefono=$request->input('telefono');
+
+        $upCliente->update();
+
+        $usuario=$request->input('nombre');
+        session()->flash('exito', 'Se ha actualizado al usuario '.$usuario);
+        return redirect()->back();
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(cliente $cliente)
+    public function destroy(Request $request, $id)
     {
-        //
+        $usuario=$request->input('txtnombre');
+
+        $delCliente= cliente::find($id);
+        $delCliente->delete();
+        
+        session()->flash('exito', 'Se ha eliminado a  '.$usuario);
+        return redirect()->back();
+
+        
     }
 }
--- IntroLaravelE/resources/views/clientes.blade.php
+++ IntroLaravelE/resources/views/clientes.blade.php
@@ -7,24 +7,43 @@
 {{-- Inicia tarjetaCliente --}}
 <div class="container mt-5 col-md-8">
 
-    <div class="card text-justify font-monospace">
-        <div class="card-header fs-5 text-primary">
-            JOHN DOE
-        </div>
+    @foreach ($consulta as $cliente)
 
-        <div class="card-body">
-            <h5 class="fw-bold">johndoe@gmail.com</h5>
-            <h5 class="fw-medium">6666666666</h5>
-            <p class="card-text fw-lighter"> </p>
-        </div>
+        <div class="card text-justify font-monospace">
+            <div class="card-header fs-5 text-primary">
+                {{$cliente->nombre}} {{$cliente->apellido}}
+            </div>
 
-        <div class="card-footer text-muted">
-            <button type="submit" class="btn btn-warning btn-sm">{{__('Actualizar')}}</button>
-            <button type="submit" class="btn btn-danger btn-sm">{{__('Eliminar')}}</button>
-        </div>
+            <div class="card-body">
+                <h5 class="fw-bold">{{$cliente->correo}}</h5>
+                <h5 class="fw-medium">{{$cliente->telefono}}</h5>
+                <p class="card-text fw-lighter"> </p>
+            </div>
 
-    </div>
-    {{-- Finaliza tarjetaCliente --}}
+            <div class="card-footer text-muted">
+                <!-- disparadores para la vista Clientes -->
+                <div class="btn-group" role="group">
+                    <button type="button" class="btn btn-outline-primary dropdown-toggle" data-bs-toggle="dropdown"
+                        aria-expanded="false">
+                        <i class="bi bi-list-stars"></i> Opciones
+                    </button>
+                    <ul class="dropdown-menu">
+                        <button type="button" class="btn btn-warning m-1" data-bs-toggle="modal"
+                            data-bs-target="#update{{$cliente->id}}">
+                            <i class="bi bi-pencil-square"></i>Editar
+                        </button>
+                        <button type="button" class="btn btn-danger m-1" data-bs-toggle="modal"
+                            data-bs-target="#destroy{{$cliente->id}}">
+                            <i class="bi bi-trash"></i> Borrar
+                        </button>
+                    </ul>
+                </div>
+            </div>
+
+        </div>
+        {{-- Finaliza tarjetaCliente --}}
+    @include('options')
+    @endforeach
 
 </div> {{-- divcontainer --}}
 
